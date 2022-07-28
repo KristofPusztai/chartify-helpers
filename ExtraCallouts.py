64489bcae5f09125a8c7a785ea9d4b22063817ac
Kristof Pusztai <kpusztai@spotify.com>
2022-07-28 17:43:38 -0400

    started

---
+++ ExtraCallouts.py
@@ -0,0 +1,19 @@
+#Extra callouts for chartify plots
+
+def circular_callouts(points, labels, colors):
+    """Parameters
+    ----------
+    points :
+    labels :
+    colors :
+
+    Examples
+    --------
+    FIXME: Add docs.
+
+
+    """
+
+    
+
+    
